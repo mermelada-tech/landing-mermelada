21662f5d2d301c4f074ca429c0899badee8dd33b
nujovich <nujovich@gmail.com>
2025-03-23 16:27:16 +0100

    feat: add svg animations

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,11 +3,6 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
-			backgroundColor: {
-				'primary': '#3490dc',
-				'secondary': '#ffed4a',
-				'danger': '#e3342f',
-			},
 		},
 	},
 	plugins: [],
