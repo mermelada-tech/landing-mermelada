af335712e6cccb5d56c8dc2f25a3d6628051c987
nujovich <nujovich@gmail.com>
2025-03-23 13:41:04 +0100

    feat: add hero background

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,13 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundColor: {
+				'primary': '#3490dc',
+				'secondary': '#ffed4a',
+				'danger': '#e3342f',
+			},
+		},
 	},
 	plugins: [],
 }
